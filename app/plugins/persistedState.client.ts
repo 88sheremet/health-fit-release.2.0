c50ba8c6bf3ae7750fda49cda0bff4cbdc7127c9
Oleksandr Sheremet <o.sheremet@viseven.com>
2026-07-24 16:27:35 +0300

    update(connect supabase bd)

--- app/plugins/persistedState.client.ts
+++
@@ -1,7 +0,0 @@
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.hook('app:created', () => {
-    nuxtApp.$pinia.use(piniaPluginPersistedstate)
-  })
-})
