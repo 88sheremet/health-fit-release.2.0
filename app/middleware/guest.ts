4ebd1384ab161371795982e0331d3ae13f2848ee
Oleksandr Sheremet <o.sheremet@viseven.com>
2026-08-18 17:14:45 +0300

    update(transfer daily progress to dsupabase bd)

--- app/middleware/guest.ts
+++ app/middleware/guest.ts
@@ -1,18 +1,15 @@
 export default defineNuxtRouteMiddleware(async () => {
   const supabase = useSupabaseClient();
 
-  // Получаем текущего пользователя
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
-  // Пользователь не авторизован —
-  // остаёмся на login/register
+
   if (!user) {
     return;
   }
 
-  // Проверяем, проходил ли пользователь скрининг
   const { data: screening, error } = await supabase
     .from("screening_results")
     .select("id")
@@ -25,16 +22,12 @@ export default defineNuxtRouteMiddleware(async () => {
       error
     );
 
-    // При ошибке не отправляем пользователя
-    // сразу на daily
     return;
   }
 
-  // Новый пользователь — скрининг ещё не проходил
   if (!screening) {
     return navigateTo("/welcome");
   }
 
-  // Скрининг уже пройден
   return navigateTo("/daily");
 });
